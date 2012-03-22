18723625ef72aebb0dc6b55ac28ba68faf829f25
Louis Dionne <louis.dionne92@gmail.com>
2012-03-21 22:59:56 -0400

    Rewrite the parser specification to fit the grammar.

--- nstl/parse.py
+++ nstl/parse.py
@@ -26,9 +26,10 @@ class NstlParser(object):
 		return self.parser.parse(text, lexer=self.lexer, **kwargs)
 	
 	def accumulate(self, p, skip=0):
-		"""This function accumulates tokens in a list. This is useful for all
-			non terminals with the following pattern.
-			non-terminal-list : non-terminal | non-terminal-list non-terminal
+		"""This function accumulates tokens in a sequence or list. This is
+		    useful for all non terminals with the following pattern.
+			non-terminal-seq : non-terminal | non-terminal-seq non-terminal
+			
 		skip : The number of tokens to skip after the first token in order to
 				get the token to accumulate. In the example, skip=0 because the
 				tokens follow each other.
@@ -37,273 +38,213 @@ class NstlParser(object):
 	
 	
 	
-	def p_input(self, p):
-		"""input : nothing
-				 | def-list
-		"""
-		p[0] = ast.Program(p[1] or [ ])
+	def p_program(self, p):
+	    """program : declaration-seq-opt
+	    """
+	    p[0] = ast.Program(p[1])
 	
+	def p_declaration_seq_opt(self, p):
+	    """declaration-seq-opt : nothing
+	                           | declaration-seq
+	    """
+	    p[0] = p[1] or [ ]
 	
+	def p_declaration_seq(self, p):
+	    """declaration-seq : declaration
+	                       | declaration-seq declaration
+	    """
+	    p[0] = self.accumulate(p)
 	
-	#
-	# Statements
-	#
-	def p_statement(self, p):
-		"""stmnt : import-stmnt
-		"""
-		p[0] = p[1]
+	def p_declaration(self, p):
+	    """declaration : namespace-definition
+	                   | template-declaration
+	    """
+	    p[0] = p[1]
 	
-	
-	
-	# Import
-	def p_import_statement(self, p):
-		"""import-stmnt : tIMPORT arg-expr-list
-		"""
-		p[0] = ast.Import(p[2])
-	
-	
-	
-	#
-	# Definitions
-	#
-	def p_definition(self, p):
-		"""def : namespace-def
-			   | template-def
-		"""
-		p[0] = p[1]
-	
-	
-	
-	# Namespace
 	def p_namespace_definition(self, p):
-		"""namespace-def : tNAMESPACE identifier tLBRACE nothing tRBRACE
-						 | tNAMESPACE identifier tLBRACE def-list tRBRACE
-		"""
-		p[0] = ast.Namespace(p[2], p[4] or [ ])
+	    """namespace-definition : tNAMESPACE identifier tLBRACE namespace-body tRBRACE
+	    """
+	    p[0] = ast.Namespace(p[2], p[4])
 	
+	def p_namespace_body(self, p):
+	    """namespace-body : nothing
+	                      | declaration-seq
+	    """
+	    p[0] = p[1] or [ ]
 	
-	
-	# Template
-	def p_template_definition(self, p):
-		"""template-def : template-begin template-params template-body
-		"""
-		p[0] = ast.Template(p[1], p[2] or [ ], p[3] or [ ])
-	
-	def p_template_begin(self, p):
-		"""template-begin : tTEMPLATE identifier
-		"""
-		p[0] = p[2]
-	
-	def p_template_params(self, p):
-		"""template-params : tLPAREN nothing tRPAREN
-						   | tLPAREN param-list tRPAREN
-		"""
-		p[0] = p[2]
+	def p_template_declaration(self, p):
+	    """template-declaration : tTEMPLATE identifier tLPAREN parameter-declaration-clause tRPAREN template-body
+	    """
+	    p[0] = ast.Template(p[2], p[4], p[6])
 	
 	def p_template_body(self, p):
-		"""template-body : tLBRACE nothing tRBRACE
-						 | tLBRACE stmnt-or-expr-list tRBRACE
-		"""
-		p[0] = p[2]
+	    """template-body : compound-statement
+	    """
+	    p[0] = p[1]
 	
+	def p_parameter_declaration_clause(self, p):
+	    """parameter-declaration-clause : nothing
+	                                    | parameter-declaration-list
+	    """
+	    p[0] = p[1] or [ ]
 	
+	def p_parameter_declaration_list(self, p):
+	    """parameter-declaration-list : parameter-declaration
+	                                  | parameter-declaration-list tCOMMA parameter-declaration
+	    """
+	    p[0] = self.accumulate(p, skip=1)
 	
-	#
-	# Expressions
-	#
-	def p_expression(self, p):
-		"""expr : postfix-expr
-		"""
-		p[0] = p[1]
+	def p_parameter_declaration(self, p):
+	    """parameter-declaration : parameter-id
+	                             | parameter-id tEQUALS expression
+	    """
+	    p[0] = ast.ParameterDeclaration(p[1], None if len(p) == 2 else p[3])
 	
+	def p_parameter_id(self, p):
+	    """parameter-id : identifier
+	                    | identifier tLPAREN identifier-list-opt tRPAREN
+	    """
+	    p[0] = ast.ParameterIdentifier(p[1], None if len(p) == 2 else p[3])
 	
+	def p_compound_statement(self, p):
+	    """compound-statement : tLBRACE statement-seq-opt tRBRACE
+	    """
+	    p[0] = ast.CompoundStatement(p[2])
 	
-	def p_postfix_expression__primary_expr(self, p):
-		"""postfix-expr : primary-expr
-		"""
-		p[0] = p[1]
+	def p_statement_seq_opt(self, p):
+	    """statement-seq-opt : nothing
+	                         | statement-seq
+	    """
+	    p[0] = p[1] or [ ]
 	
-	def p_postfix_expression__scope_op(self, p):
-		"""postfix-expr : postfix-expr tPERIOD identifier
-		"""
-		p[0] = ast.ScopeOp(p[1], p[3])
+	def p_statement_seq(self, p):
+	    """statement-seq : statement
+	                     | statement-seq statement
+	    """
+	    p[0] = self.accumulate(p)
 	
-	def p_postfix_expression__call_op(self, p):
-		"""postfix-expr : postfix-expr tLPAREN nothing tRPAREN
-						| postfix-expr tLPAREN arg-list tRPAREN
-		"""
-		p[0] = ast.CallOp(p[1], p[3] or [ ])
+	def p_statement(self, p):
+	    """statement : import-statement
+	                 | nest-statement
+	                 | expression-statement
+	    """
+	    p[0] = p[1]
 	
+	def p_nest_statement(self, p):
+	    """nest-statement : tNEST id-expression-list with-clause-opt
+	    """
+	    p[0] = ast.NestStatement(p[2], p[3])
 	
-	def p_primary_expression__ident(self, p):
-		"""primary-expr : identifier
-		"""
-		p[0] = p[1]
+	def p_import_statement(self, p):
+	    """import-statement : tIMPORT id-expression-list with-clause-opt
+	    """
+	    p[0] = ast.ImportStatement(p[2], p[3])
 	
-	def p_primary_expression__rawinput(self, p):
-		"""primary-expr : rawinput
-		"""
-		p[0] = p[1]
+	def p_expression_statement(self, p):
+	    """expression-statement : expression
+	    """
+	    p[0] = p[1]
 	
-	def p_primary_expression__expr(self, p):
-		"""primary-expr : tLPAREN expr tRPAREN
-		"""
-		p[0] = p[2]
+	def p_with_clause_opt(self, p):
+	    """with-clause-opt : nothing
+	                       | with-clause
+	    """
+	    p[0] = p[1] or [ ]
 	
-	
-	
-	#
-	# Others
-	#
-	def p_argument_list(self, p):
-		"""arg-list : arg
-					| arg-list tCOMMA arg
-		"""
-		p[0] = self.accumulate(p, skip=1)
-		if len(p[0]) > 1 and p[0][0].haskeyword() and not p[0][1].haskeyword():
-			raise ParseError("non-keyword argument follows keyword argument")
-	
-	def p_argument(self, p):
-		"""arg : expr
-			   | param-id tEQUALS expr
-		"""
-		if len(p) > 2:
-			p[0] = ast.Argument(p[3], p[1])
-		else:
-			p[0] = ast.Argument(p[1], None)
-	
-	
-	
-	def p_parameter_list(self, p):
-		"""param-list : param
-					  | param-list tCOMMA param
-		"""
-		p[0] = self.accumulate(p, skip=1)
-		if len(p[0]) > 1 and p[0][0].hasdefault() and not p[0][1].hasdefault():
-			raise ParseError("non-default argument follows default argument")
-	
-	def p_parameter(self, p):
-		"""param : param-id
-				 | param-id tEQUALS expr
-		"""
-		p[0] = ast.Parameter(p[1], p[3] if len(p) > 2 else None)
-	
-	def p_parameter_identifier(self, p):
-		"""param-id : tID param-id-params
-		"""
-		p[0] = ast.ParameterId(p[1], p[2])
-	
-	def p_parameter_identifier_params(self, p):
-		"""param-id-params : nothing
-						   | tLPAREN nothing tRPAREN
-						   | tLPAREN tID-list tRPAREN
-		"""
-		p[0] = p[1] if len(p) == 2 else (p[2] or [ ])
-	
-	def p_parameter_identifier_params_tID_list(self, p):
-		"""tID-list : tID
-					| tID-list tCOMMA tID
-		"""
-		p[0] = self.accumulate(p, skip=1)
-	
-	
-	
-	def p_identifier(self, p):
-		"""identifier : tID
-		"""
-		p[0] = ast.Identifier(p[1])
-	
-	def p_identifier_list(self, p):
-		"""identifier-list : identifier
-						   | identifier-list tCOMMA identifier
-		"""
-		p[0] = self.accumulate(p, skip=1)
-	
-	
-	
-	def p_rawinput(self, p):
-		"""rawinput : tRAWBEGIN nothing tRAWEND
-					| tRAWBEGIN rawinput_aux tRAWEND
-		"""
-		p[0] = ast.Raw("" if p[2] is None else "".join(p[2]))
-	
-	def p_rawinput_aux(self, p):
-		"""rawinput_aux : tRAWINPUT
-						| rawinput_aux tRAWINPUT
-		"""
-		p[0] = self.accumulate(p)
-	
-	
-	
-	def p_nothing(self, p):
-		"""nothing : 
-		"""
-		p[0] = None
-	
-	
-	
-	#
-	# Helpers
-	#
-	def p_statement_list(self, p):
-		"""stmnt-list : stmnt
-					  | stmnt-list stmnt
-		"""
-		p[0] = self.accumulate(p)
-	
-	
-	
-	def p_definition_list(self, p):
-		"""def-list : def
-					| def-list def
-		"""
-		p[0] = self.accumulate(p)
-	
-	
-	
-	def p_expression_list(self, p):
-		"""expr-list : expr
-					 | expr-list expr
-		"""
-		p[0] = self.accumulate(p)
-	
-	
+	def p_with_clause(self, p):
+	    """with-clause : tWITH argument-expression-list
+	    """
+	    p[0] = p[2]
 	
 	def p_argument_expression_list(self, p):
-		"""arg-expr-list : expr
-						 | arg-expr-list tCOMMA expr
-		"""
-		p[0] = self.accumulate(p, skip=1)
+	    """argument-expression-list : argument-expression
+	                                | argument-expression-list tCOMMA argument-expression
+	    """
+	    p[0] = self.accumulate(p, skip=1)
 	
+	def p_argument_expression(self, p):
+	    """argument-expression : parameter-id tEQUALS expression
+	    """
+	    p[0] = ast.ArgumentExpression(p[1], p[3])
 	
+	def p_id_expression_list(self, p):
+	    """id-expression-list : id-expression
+	                          | id-expression-list tCOMMA id-expression
+	    """
+	    p[0] = self.accumulate(p, skip=1)
 	
-	def p_definition_or_statement(self, p):
-		"""def-or-stmnt : def
-						| stmnt
-		"""
-		p[0] = p[1]
+	def p_expression(self, p):
+	    """expression : tLPAREN expression tRPAREN
+	                  | id-expression
+	                  | raw-expression
+	    """
+	    p[0] = p[1] if len(p) == 2 else p[2]
 	
-	def p_definition_or_statement_list(self, p):
-		"""def-or-stmnt-list : def-or-stmnt
-							 | def-or-stmnt-list def-or-stmnt
-		"""
-		p[0] = self.accumulate(p)
+	def p_raw_expression(self, p):
+	    """raw-expression : tRAWBEGIN raw-input-opt tRAWEND
+	    """
+	    p[0] = ast.RawExpression(p[2])
 	
+	def p_raw_input_opt(self, p):
+	    """raw-input-opt : nothing
+	                     | raw-input
+	    """
+	    p[0] = p[1] or ""
 	
+	def p_raw_input(self, p):
+	    """raw-input : tRAWINPUT
+	                 | raw-input tRAWINPUT
+	    """
+	    p[0] = p[1] + (p[2] if len(p) > 2 else '')
 	
-	def p_statement_or_expression(self, p):
-		"""stmnt-or-expr : stmnt
-						 | expr
-		"""
-		p[0] = p[1]
+	def p_id_expression(self, p):
+	    """id-expression : unqualified-id
+	                     | qualified-id
+	    """
+	    p[0] = p[1]
 	
-	def p_statement_or_expression_list(self, p):
-		"""stmnt-or-expr-list : stmnt-or-expr
-							  | stmnt-or-expr-list stmnt-or-expr
-		"""
-		p[0] = self.accumulate(p)
+	def p_unqualified_id(self, p):
+	    """unqualified-id : identifier
+	    """
+	    p[0] = p[1]
 	
+	def p_qualified_id(self, p):
+	    """qualified-id : nested-name-specifier tPERIOD unqualified-id
+	    """
+	    p[0] = ast.QualifiedIdentifier(reversed(p[1]), p[3])
 	
+	def p_nested_name_specifier(self, p):
+	    """nested-name-specifier : namespace-name
+	                             | nested-name-specifier tPERIOD namespace-name
+	    """
+	    p[0] = self.accumulate(p, skip=1)
+	
+	def p_namespace_name(self, p):
+	    """namespace-name : identifier
+	    """
+	    p[0] = p[1]
+	
+	def p_identifier(self, p):
+	    """identifier : tID
+	    """
+	    p[0] = ast.Identifier(p[1])
+	
+	def p_identifier_list_opt(self, p):
+	    """identifier-list-opt : nothing
+	                           | identifier-list
+	    """
+	    p[0] = p[1] or [ ]
+	
+	def p_identifier_list(self, p):
+	    """identifier-list : identifier
+	                       | identifier-list tCOMMA identifier
+	    """
+	    p[0] = self.accumulate(p, skip=1)
+	
+	def p_nothing(self, p):
+	    """nothing : 
+	    """
+	    p[0] = None
 	
 	def p_error(self, p):
 		raise ParseError("{}: token [{}] with type [{}]"
